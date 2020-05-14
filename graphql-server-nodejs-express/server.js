b4898d482cbf5654fdc2666e9d4eb47a6fdbe821
Ahmed Rebai <Ahmed.Rebai@ALMERYS.Local>
2020-05-14 13:02:29 +0100

    Poc graphql with express-graphql

---
+++ graphql-server-nodejs-express/server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require('graphql');
+// Build the schema //
+var schema = buildSchema(
+    `
+   type Query {
+       hello:String
+   } 
+    
+    `
+)
+
+var root = {
+    hello: () => {
+      return 'Hello world!';
+    },
+  };
+
+var app = express();
+app.use('/graphql', graphqlHTTP({
+    schema :schema,
+    rootValue:root,
+    graphiql:true
+}))
+
+app.listen(4000);
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
